c533666d890cacdbeb3231a0f85d19aa3321cb34
victordepaularosa <115742655+victordepaularosa@users.noreply.github.com>
2023-02-21 17:00:01 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from menu import *
+from dias_uteis import *
+from salario_mensal import *
+
+# Função para limpar tela
+limpar_tela()
+
+# Menu Inicial
+menu = Menu()
+menu.menu_inicial()
+# Informações inseridas pelo Usuário
+mes = menu.mes_referencia()
+ano = menu.ano_referencia()
+estado = menu.estado_referencia()
+hora_trabalho = menu.hora_trabalho_referencia()
+
+# Geração do calendário do funcionário
+calendario = Calendario_Br(mes, ano, estado)
+
+# Função para limpar tela
+limpar_tela()
+
+# Cálculo das horas trabalhadas pelo funcionário no mês
+funcionario = Salario_mensal(hora_trabalho, len(calendario))
+
+# Imprimir as Informações úteis para o usuário
+menu.menu_inicial()
+calendario.imprime_dias_uteis_mes()
+funcionario.imprime_horas_mes()
+funcionario.imprime_salario_mensal()
